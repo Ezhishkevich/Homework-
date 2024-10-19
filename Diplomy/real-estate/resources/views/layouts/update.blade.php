e79d53d5c568b46be2fa1875d245fdd55eb453ff
Ezhishkevich <156563377+Ezhishkevich@users.noreply.github.com>
2024-10-19 14:53:08 +0300

    Update update.blade.php

--- Diplomy/real-estate/resources/views/layouts/update.blade.php
+++ Diplomy/real-estate/resources/views/layouts/update.blade.php
@@ -12,15 +12,6 @@
                         <h4 class="m-0">@yield('page.titel')</h4>
                     </div>
                     <div class="card-body">
-                        @if($errors->any())
-                        <div class="alert-dang">
-                            <ul>
-                                @foreach($errors->all() as $error)
-                                <li>{{$error}}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                        @endif
                         <form action="{{ route('updatesubmit', $article->id) }}" method="post">
                             @csrf
                             <div class="mb-3 ">
